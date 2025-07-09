52fa1e84a2100045dc190e8245f2fdc0ffd22eba
Camilo López <camiloj.lopez79@gmail.com>
2025-07-08 21:05:00 -0400

    [CONFIG] Setup envs config

--- src/common/env/env.ts
+++ src/common/env/env.ts
@@ -1,8 +1,9 @@
 type Env = {
   API_URI: string;
-  LOG_LEVEL: 'debug' | 'info' | 'warm' | 'error';
+  LOG_LEVEL: 'debug' | 'info' | 'warn' | 'error';
 }
 
 export const env: Env = {
-  API_URI = process.env.NEXT_PUBLIC_API_URL ?? 'http://localhost:3000'
+  API_URI: process.env.NEXT_PUBLIC_API_URL ?? 'http://localhost:3000',
+  LOG_LEVEL: (process.env.NEXT_PUBLIC_LOG_LEVEL as Env['LOG_LEVEL']) ?? 'info'
 }
